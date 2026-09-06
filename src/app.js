c394716c045868998c5def2b6054f2ae1a07cac4
Marion Acaso <marionberi838@gmail.com>
2026-09-06 23:39:17 +0800

    refactor: restructure project with new server setup and PostgreSQL integration

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import studentRoutes from "./routes/studentRoutes.js";
+const app = express();
+
+app.use(express.json());
+
+app.use("/students", studentRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "server is running",
+  });
+});
+
+export default app;
